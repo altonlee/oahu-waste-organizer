35ab7329d1f41200a93ce9bf4cdf399647c4480c
Emily Pang <eppang@hawaii.edu>
2018-11-12 23:45:54 -1000

    Updated category dropdown menu

--- app/imports/ui/pages/InputData.jsx
+++ app/imports/ui/pages/InputData.jsx
@@ -4,13 +4,22 @@ import '/client/input.css';
 
 class InputData extends React.Component {
   render() {
+    const categoryOptions = [
+      { key: 'items of interest', text: '---Items of Interest---', value: 'items of interest', disabled: true },
+      { key: 'starbucks cups', text: 'Starbucks Cups', value: 'starbucks cups' },
+      { key: 'plastic to-go cups', text: 'Plastic To-Go Cups', value: 'plastic to-go cups' },
+      { key: 'wax paper cups', text: 'Wax Paper Cups', value: 'wax paper cups' },
+      { key: 'compostable utensils', text: 'Compostable Utensils', value: 'compostable utensils' },
+      { key: 'straws', text: 'Straws', value: 'straws' },
+    ];
+
     return (
         <div>
-          <Grid container columns="two" className="border-margin-border-margin-border-margin-border-margin-border-margin-border-margin">
+          <Grid container columns="two" className="border-margin">
             <Grid.Row>
               <Grid.Column>
                 <div className="form-heading">Location</div>
-                <Dropdown placeholder='Select Campus' fluid search selection/>
+                <Dropdown placeholder='Select Campus' fluid search selection />
                 <div className="form-heading">Building</div>
                 <Dropdown placeholder='Select Building' fluid search selection/>
                 <div className="form-heading">Notes</div>
@@ -27,9 +36,9 @@ class InputData extends React.Component {
           <div className="card-margins">
             <Card fluid>
               <Header as='h3'>Category</Header>
-              <Dropdown placeholder='Select Category' fluid search selection/>
+              <Dropdown placeholder='Select Category' selection search options={categoryOptions} />
               <div className="card-margins">
-                <Card fluid>
+                <Card>
                   <Header as='h3'>Weight</Header>
                   <Input placeholder="Weight"/>
                 </Card>
