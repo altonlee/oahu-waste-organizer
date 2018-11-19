f72cd5d39d12b910b91b3e70544ec2e5fc48805b
Alton Lee <altonlee@hawaii.edu>
2018-11-18 14:40:25 -1000

    Added loader to Events, added padding to trapezoid shape on Landing

--- app/imports/ui/pages/ListEvents.jsx
+++ app/imports/ui/pages/ListEvents.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { Container, Item, Header } from 'semantic-ui-react';
+import { Container, Item, Header, Loader } from 'semantic-ui-react';
 import { Events } from '/imports/api/event/event';
 import Event from '/imports/ui/components/Event';
 
 /** Renders a table containing all of the Event documents. Use <Event> to render each row. */
 class ListEvents extends React.Component {
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+  
+  renderPage() {
     const eventStyle = { paddingBottom: '15px' };
     return (
         <Container style={eventStyle}>
@@ -24,6 +28,7 @@ class ListEvents extends React.Component {
 /** Require an array of Events in the props. */
 ListEvents.propTypes = {
   event: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
